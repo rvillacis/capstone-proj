59129e8a6b70cbc4e34d25535dae6b75d6450f7c
rvillacis <romuloavillacisc@hotmail.com>
2021-10-07 09:32:27 -0500

    Extraction Updated

--- extraction.py
+++ extraction.py
@@ -8,7 +8,7 @@ class Currency_data():
 
         self.fin_data = pd.read_csv(self.filename,index_col='DATE',parse_dates=True)
 
-    def batch(self,start=None,end=None,days=None,months=None,years=None,currencies=None):
+    def batch(self,start=None,end=None,days=0,months=0,years=0,currencies=None):
         self.start = start
         self.end = end
         self.days = days
@@ -16,23 +16,57 @@ class Currency_data():
         self.years = years
         self.currencies = currencies
 
+        if (self.start == None) and (self.end == None):
+                raise ValueError('You need to provide a start date, end date, or both.')
+
+        if (self.end != None) and (self.start == None):
+            self.days,self.months,self.years = -self.days,-self.months,-self.years
+
+        if (self.start != None) and (self.end != None):
+            if self.end > self.start:
+                return self.fin_data[self.start:self.end]
+            else:
+                raise Exception('The end date must be after the start date')
+            
         if self.start != None:
+            year, month, day = self.start.split('-')
+        else:
+            year, month, day = self.end.split('-')
+        year, month, day = int(year), int(month), int(day)
+
+        if self.years != 0:
+            year = year + self.years
+
+        if self.months != 0:
+            if (month + self.months) > 12:
+                month = 1 + ((month + self.months) % 12)
+                year = year + ((month + self.months) // 12)
+            elif (month + self.months) < 1:
+                month = 12 - ((month - self.months) % 12)
+                year = year - ((month - self.months + 1) // 12)
+            else:
+                month = month + self.months
+
+        if self.days != 0:
+            if self.start != None:
+                cropped_data = self.fin_data[self.start:]
+                return cropped_data[:self.days]
             if self.end != None:
-                return self.fin_data[start:end]
-            elif days != None:
-                data = self.fin_data[start:]
-                return data[:days]
-            elif months != None:
-                year, month, day = start.split('-')
-                # bye, chao, todo = int(bye), int(chao), int(todo)
-                #You have to consider what if it rolls to the next year
-            elif years != None:
-                year, month, day = start.split('-')
-                new_year = int(year) + years
-                end_date = '{}-{}-{}'.format(new_year,month,day)
-                return self.fin_data[start:end_date]
+                cropped_data = self.fin_data[:self.end]
+                return cropped_data[self.days:]
+
+        start_end_date = '{}-{}-{}'.format(year,month,day)
+        if self.start != None:
+            return self.fin_data[self.start:start_end_date]
+        if self.end != None:
+            return self.fin_data[start_end_date:self.end]
+
+
+#log_returns - maybe do this a a feature, add number of days or columns as features
+#random _times
+# and then let's do indicators
 
 
 if __name__ == '__main__':
     hola = Currency_data()
-    hola.batch('1992-01-01',years=1)
+    print(hola.batch(end='1996-01-01',years=1))
