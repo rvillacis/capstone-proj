c5ec1bcbc66d2897b822ad3ebb72ac6d5abd6446
rvillacis <romuloavillacisc@hotmail.com>
2021-10-11 15:41:41 -0500

    Added log returns

--- extraction.py
+++ extraction.py
@@ -1,14 +1,16 @@
 class Currency_data():
     
     def __init__(self):
-        self.filename = 'FX_Test_USD-per-FX_Chicago.csv'
 
-        import numpy as np
         import pandas as pd
 
+        self.filename = 'FX_Test_USD-per-FX_Chicago.csv'
         self.fin_data = pd.read_csv(self.filename,index_col='DATE',parse_dates=True)
 
     def batch(self,start=None,end=None,days=0,months=0,years=0,currencies=None):
+
+        import numpy as np
+
         self.start = start
         self.end = end
         self.days = days
@@ -24,7 +26,7 @@ class Currency_data():
 
         if (self.start != None) and (self.end != None):
             if self.end > self.start:
-                return self.fin_data[self.start:self.end]
+                self.fin_data= self.fin_data[self.start:self.end]
             else:
                 raise Exception('The end date must be after the start date')
             
@@ -50,23 +52,43 @@ class Currency_data():
         if self.days != 0:
             if self.start != None:
                 cropped_data = self.fin_data[self.start:]
-                return cropped_data[:self.days]
+                self.fin_data = cropped_data[:self.days]
             if self.end != None:
                 cropped_data = self.fin_data[:self.end]
-                return cropped_data[self.days:]
+                self.fin_data = cropped_data[self.days:]
+        else:
+            start_end_date = '{}-{}-{}'.format(year,month,day)
+            if self.start != None:
+                self.fin_data= self.fin_data[self.start:start_end_date]
+            if self.end != None:
+                self.fin_data = self.fin_data[start_end_date:self.end]
 
-        start_end_date = '{}-{}-{}'.format(year,month,day)
-        if self.start != None:
-            return self.fin_data[self.start:start_end_date]
-        if self.end != None:
-            return self.fin_data[start_end_date:self.end]
+        if (type(self.currencies) == str) or (type(self.currencies) == list):
+            self.fin_data = self.fin_data[self.currencies]
+        elif self.currencies == None:
+            pass
+        else:
+            raise Exception('Please enter a string/list for the currencies you want to analyze or leave empty.')
 
+        try:
+            self.fin_data = self.fin_data.to_frame()
+        except:
+            pass
 
-#log_returns - maybe do this a a feature, add number of days or columns as features
+        self.num_rows = len(self.fin_data)
+        self.num_cols = len(self.fin_data.columns)
+        self.px_change = self.fin_data - self.fin_data.shift(1)
+        self.pct_change = self.fin_data.pct_change()
+        self.log_ret = np.log1p(self.pct_change)
+
+        return self.fin_data
+
 #random _times
 # and then let's do indicators
+#Should currency just be the base class and batch another object that inherits from it
 
 
 if __name__ == '__main__':
     hola = Currency_data()
-    print(hola.batch(end='1996-01-01',years=1))
+    data = hola.batch(end='1997-01-01',days=30,currencies=['USDEUR','USDGBP'])
+    print(hola.log_ret)
