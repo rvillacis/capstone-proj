9f98abae00e3f7da62e962647383b21499b4a52b
rvillacis <romuloavillacisc@hotmail.com>
2021-11-22 21:49:58 -0600

    Updated portfolio creation

--- Trading/portfolio.py
+++ Trading/portfolio.py
@@ -1,13 +1,15 @@
 import numpy as np
 import pandas as pd
-from .extraction import Batch, Random_batch
+from extraction import Batch, Random_batch
 
 class Portfolio():
 
     def __init__(self,prices,cash=1000000,positions=None,benchmark=None):
 
         self.cash = cash
+        self.max_portfolio = cash
         self.num_orders = 0
+        self.pending_orders = {'target_orders':[], 'stop_losses':[]}
 
         if (isinstance(prices, pd.DataFrame)) or (isinstance(prices, Batch)):
             if isinstance(prices, Batch):
@@ -38,14 +40,8 @@ class Portfolio():
 
         self.transactions = pd.DataFrame(columns=['amount','commission','dt','order_id','price','sid','symbol','txn_dollars'])
 
-    def purchase_order(self,symbol,amount,price,date):
+    def check_cash(self,symbol,amount,price,date):
 
-        if symbol not in self.positions:
-            raise ValueError('This symbol is not in the price data passed')
-
-        if date not in self.positions.index:
-            raise ValueError('Date {} is not in the data passed'.format(date))
-        
         if self.positions.loc[date,symbol] == 0:
             if self.positions.loc[date,'cash'] < abs(amount*price):
                 raise ValueError('There is not enough available cash to buy {} {} on {}'.format(amount,symbol,date))
@@ -55,6 +51,46 @@ class Portfolio():
         elif self.positions.loc[date,symbol] < 0:
             if (-amount*price > self.positions.loc[date,'cash']) or (amount*price > self.cash):
                 raise ValueError('There is not enough available cash to buy {} {} on {}'.format(amount,symbol,date))
+
+    def kelly_criterion(self,current_price,expected_price,currency,date):
+        
+        expected_return = 1 + (abs(expected_price - current_price)/current_price)
+        favorable_probability = self.model_success_probability.loc[currency]
+        current_position = abs(current_price * self.positions.loc[date,currency])
+
+        position_size_usd = (expected_return * favorable_probability + favorable_probability - 1) / (expected_return * max(np.log(current_position)/2,1))
+
+        return position_size_usd
+
+    def execute_pending_orders(self,df_actual,date):
+    
+        for order in target_orders:
+            if order['long_short'] == 'long':
+                if df_actual[date,order['currency']] >= order['target']:
+                    portfolio.purchase_order(order['currency'],-order['amount'], df_actual[date,order['currency']], date)
+
+            elif order['long_short'] == 'short':
+                if df_actual[date,order['currency']] <= order['target']:
+                    portfolio.purchase_order(order['currency'],-order['amount'], df_actual[date,order['currency']], date)
+
+        for order in stop_losses:
+            if order['long_short'] == 'long':
+                if df_actual[date,order['currency']] <= order['stop']:
+                    portfolio.purchase_order(order['currency'],-order['amount'], df_actual[date,order['currency']], date)
+
+            elif order['long_short'] == 'short':
+                if df_actual[date,order['currency']] >= order['stop']:
+                    portfolio.purchase_order(order['currency'],-order['amount'], df_actual[date,order['currency']], date)
+
+    def purchase_order(self,symbol,amount,price,date):
+
+        if symbol not in self.positions:
+            raise ValueError('This symbol is not in the price data passed')
+
+        if date not in self.positions.index:
+            raise ValueError('Date {} is not in the data passed'.format(date))
+        
+        self.check_cash(symbol,amount,price,date)
 
         self.num_orders += 1
 
@@ -79,6 +115,38 @@ class Portfolio():
             self.positions.loc[date:,'cash'] += txn_dollars
 
         self.positions.loc[date:,symbol] += amount
+
+    def set_stop_loss(self,currency,units,stop_price):
+
+        long_short = 'long' if units >= 0 else 'short'
+
+        stop_loss = {
+            'currency': currency,
+            'long_short': long_short,
+            'amount': units,
+            'stop': stop_price
+        }
+
+        self.pending_orders['stop_losses'].append(stop_loss)
+
+    def set_target_order(self,currency,units,target_price):
+        
+        long_short = 'long' if units >= 0 else 'short'
+
+        target_order = {
+            'currency': currency,
+            'long_short': long_short,
+            'amount': units,
+            'target': target_price
+        }
+
+        self.pending_orders['target_orders'].append(target_order)
+
+    def long_biggest_winner():
+        pass
+
+    def short_biggest_loser():
+        pass
         
     def calc_returns(self):
 
@@ -102,8 +170,6 @@ class Portfolio():
 
         return returns,positions,transactions
 
-    def generate_random_port(self):
-        pass
 
 if __name__ == '__main__':
 
@@ -113,5 +179,9 @@ if __name__ == '__main__':
     portfolio.purchase_order('USDEUR',-500000, 1.0559, '2003-01-15')
     # portfolio.calc_returns()
 
-    returns,positions,transactions = portfolio.backtest_data()
-    print(transactions.index)
+    # returns,positions,transactions = portfolio.backtest_data()
+
+    # data = {'price':20}
+    # hola = data['prie'] or 0
+
+    print(portfolio.positions)
